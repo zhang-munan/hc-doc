08291c293cc21c3db1b03161abf1d3d0eb1ebf34
zhang-munan <zhangmunan_966@163.com>
2022-11-19 22:09:02 +0800

    新增小程序说明

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,13 +5,12 @@ import dayjs from "dayjs";
 import sidebarOption from "../.vuepress/config/sidebar";
 
 const DOMAIN_NAME = "xugaoyi.com"; // 域名 (不带https)
-const WEB_SITE = `https://${DOMAIN_NAME}`; // 网址
+const WEB_SITE = `https://${DOMAIN_NAME}`; // 网址+
 
 export default defineConfig4CustomTheme<VdoingThemeConfig>({
-  // theme: 'vdoing', // 使用npm主题包
   theme: resolve(__dirname, "../../vdoing"), // 使用本地主题包
 
-  base: '/hc-doc/',
+  base: "/hc-doc/",
 
   locales: {
     "/": {
@@ -69,7 +68,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
           { text: "Git使用手册", link: "" },
           { text: "Markdown使用教程", link: "" },
           { text: "npm常用命令", link: "" },
-          { text: "centos相关", link: "" },
+          { text: "centos相关", link: "" }
         ]
       }
     ],
@@ -112,68 +111,68 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     [
       "sitemap", // 网站地图
       {
-        hostname: WEB_SITE,
-      },
+        hostname: WEB_SITE
+      }
     ],
 
     // 可以添加第三方搜索链接的搜索框（继承原官方搜索框的配置参数）
     [
-      'thirdparty-search',
+      "thirdparty-search",
       {
         thirdparty: [
           {
-            title: '在MDN中搜索',
-            frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
-            behindUrl: '', // 搜索链接的后面部分，可选，默认 ''
+            title: "在MDN中搜索",
+            frontUrl: "https://developer.mozilla.org/zh-CN/search?q=", // 搜索链接的前面部分
+            behindUrl: "" // 搜索链接的后面部分，可选，默认 ''
           },
           {
-            title: '在Runoob中搜索',
-            frontUrl: 'https://www.runoob.com/?s=',
+            title: "在Runoob中搜索",
+            frontUrl: "https://www.runoob.com/?s="
           },
           {
-            title: '在Vue API中搜索',
-            frontUrl: 'https://cn.vuejs.org/v2/api/#',
+            title: "在Vue API中搜索",
+            frontUrl: "https://cn.vuejs.org/v2/api/#"
           },
           {
-            title: '在Bing中搜索',
-            frontUrl: 'https://cn.bing.com/search?q=',
+            title: "在Bing中搜索",
+            frontUrl: "https://cn.bing.com/search?q="
           },
           {
-            title: '通过百度搜索本站的',
-            frontUrl: `https://www.baidu.com/s?wd=site%3A${DOMAIN_NAME}%20`,
-          },
-        ],
+            title: "通过百度搜索本站的",
+            frontUrl: `https://www.baidu.com/s?wd=site%3A${DOMAIN_NAME}%20`
+          }
+        ]
       }
     ],
 
     [
-      'one-click-copy', // 代码块复制按钮
+      "one-click-copy", // 代码块复制按钮
       {
         copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
-        copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
+        copyMessage: "复制成功", // default is 'Copy successfully and then paste it for use.'
         duration: 1000, // prompt message display time.
-        showInMobile: false, // whether to display on the mobile side, default: false.
-      },
+        showInMobile: false // whether to display on the mobile side, default: false.
+      }
     ],
 
     [
-      'demo-block', // demo演示模块 https://github.com/xiguaxigua/vuepress-plugin-demo-block
+      "demo-block", // demo演示模块 https://github.com/xiguaxigua/vuepress-plugin-demo-block
       {
         settings: {
           jsfiddle: false, // 是否显示 jsfiddle 链接
           codepen: true, // 是否显示 codepen 链接
-          horizontal: false, // 是否展示为横向样式
-        },
-      },
+          horizontal: false // 是否展示为横向样式
+        }
+      }
     ],
     [
-      'vuepress-plugin-zooming', // 放大图片
+      "vuepress-plugin-zooming", // 放大图片
       {
-        selector: '.theme-vdoing-content img:not(.no-zoom)', // 排除class是no-zoom的图片
+        selector: ".theme-vdoing-content img:not(.no-zoom)", // 排除class是no-zoom的图片
         options: {
-          bgColor: 'rgba(0,0,0,0.6)',
-        },
-      },
+          bgColor: "rgba(0,0,0,0.6)"
+        }
+      }
     ],
     [
       "@vuepress/last-updated", // "上次更新"时间格式
@@ -190,7 +189,87 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         sidebarLinkSelector: ".sidebar-link",
         headerAnchorSelector: ".header-anchor"
       }
-    ]
+    ],
+
+    "@vuepress/nprogress",
+
+    [
+      "vuepress-plugin-container",
+      {
+        type: "note",
+        defaultTitle: {
+          "/": "笔记",
+          "/en/": "NOTE"
+        }
+      }
+    ],
+    [
+      "vuepress-plugin-container",
+      {
+        type: "tip",
+        defaultTitle: {
+          "/": "提示",
+          "/en/": "TIP"
+        }
+      }
+    ],
+    [
+      "vuepress-plugin-container",
+      {
+        type: "warning",
+        defaultTitle: {
+          "/": "注意",
+          "/en/": "WARNING"
+        }
+      }
+    ],
+    [
+      "vuepress-plugin-container",
+      {
+        type: "danger",
+        defaultTitle: {
+          "/": "警告",
+          "/en/": "WARNING"
+        }
+      }
+    ],
+    [
+      "vuepress-plugin-container",
+      {
+        type: "right",
+        defaultTitle: ""
+      }
+    ],
+    [
+      "vuepress-plugin-container",
+      {
+        type: "theorem",
+        before: info => `<div class="custom-block theorem"><p class="title">${info}</p>`,
+        after: "</div>"
+      }
+    ],
+    [
+      "vuepress-plugin-container",
+      {
+        type: "details",
+        before: info => `<details class="custom-block details">${info ? `<summary>${info}</summary>` : ""}\n`,
+        after: () => "</details>\n",
+        defaultTitle: {
+          "/": "点击查看",
+          "/en/": "DETAILS"
+        }
+      }
+    ],
+
+    // 内容居中容器
+    [
+      "vuepress-plugin-container",
+      {
+        type: "center",
+        before: info => `<div class="center-container">`,
+        after: () => "</div>"
+      }
+    ],
   ],
 
   markdown: {
